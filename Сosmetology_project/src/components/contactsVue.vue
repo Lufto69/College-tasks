9268c9e4edea95f97e763afa9ce2df9ebf5a9d8d
Lufto <sashatsar11@mail.ru>
2023-11-10 20:40:26 +0400

    adaptashon+one_active_bug

--- Сosmetology_project/src/components/contactsVue.vue
+++ Сosmetology_project/src/components/contactsVue.vue
@@ -17,5 +17,10 @@
                 </h1>
             </div>
         </div>
+        <div class="main_information_container">
+            <div class="map_Home">
+                <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60598.28863242853!2d50.800099620253185!3d61.66459023934711!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x43f62b36a71ed743%3A0x743826b91a7ef615!2z0JzQtdC20LTRg9C90LDRgNC-0LTQvdGL0Lkg0LDRjdGA0L7Qv9C-0YDRgiDQodGL0LrRgtGL0LLQutCw0YAg0LjQvNC10L3QuCDQny7QkC7QmNGB0YLQvtC80LjQvdCw!5e0!3m2!1sru!2sru!4v1698914882939!5m2!1sru!2sru" width="100%" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
     </div>
 </template>
