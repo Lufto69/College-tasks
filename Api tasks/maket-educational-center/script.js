9bf279418f0ab633eee62f0f55cd87aea74754f1
Lufto <60296030+Lufto69@users.noreply.github.com>
2024-01-07 19:37:06 +0400

    EMCO project

--- Api tasks/maket-educational-center/script.js
+++ Api tasks/maket-educational-center/script.js
@@ -1,3 +1,5 @@
+//элементы news
+
 const cont = document.querySelector("#container");
 const temp = document.querySelector("#news_it");
 
@@ -8,7 +10,7 @@ cont.append(create);
 create = temp.content.cloneNode(true);
 cont.append(create);
 
-
+//открытие aside
 
 let but_one = document.querySelector("#but_one");
 let but_two = document.querySelector("#but_two");
@@ -28,3 +30,21 @@ function style_close(){
 
 but_one.onclick = style_open;
 but_two.onclick = style_close;
+
+//открытие  menu
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('#burger');
+
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('burg_menu_open');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            menu.classList.toggle('burg_menu_open');
+        })
+    })
+})
