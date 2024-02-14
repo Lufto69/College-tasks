380f4be0130d23ee5c26a9514d9b79692bf8a13c
Lufto <sashatsar11@mail.ru>
2024-02-15 01:04:52 +0400

    js

--- Api tasks/magasin-cars/script.js
+++ Api tasks/magasin-cars/script.js
@@ -1,39 +1,53 @@
-let x;
+let but = document.querySelectorAll(".cards__info_but"),
+    cont = document.querySelector(".menu__reg_item > span"),
+    t = "jt",
+    item = 0;
 
-function calc(){
-    let a = Number(document.querySelector("#one").value),
-        b = Number(document.querySelector("#two").value),
-        otv;
+but.forEach(button => {
+  button.addEventListener("click", () => {
+    cont.innerHTML = (++item);
+    but.innerHTML = ;
+  })
 
-    switch (x){
-        case '1':{
-            document.querySelector("#otv").innerHTML = (a + b);
-            break
-        }
-        case '2':{
-            document.querySelector("#otv").innerHTML = (a * b);
-            break
-        }
-        case '3':{
-            document.querySelector("#otv").innerHTML = (a ** b);
-            break
-        }
-        case '4':{
-            document.querySelector("#otv").innerHTML = (a / b);
-            break
-        }
-        case '5':{
-            document.querySelector("#otv").innerHTML = (a - b);
-            break
-        }
-        case '6':{
-            document.querySelector("#otv").innerHTML = ((a/100) * b);
-            break
-        }
-        case '7':{
-            document.querySelector("#otv").innerHTML = (Math.sqrt(a));
-            break
-        }
-    }
-}
+})
 
+
+
+// let x;
+
+// function calc(){
+//     let a = Number(document.querySelector("#one").value),
+//         b = Number(document.querySelector("#two").value),
+//         otv;
+
+//     switch (x){
+//         case '1':{
+//             document.querySelector("#otv").innerHTML = (a + b);
+//             break
+//         }
+//         case '2':{
+//             document.querySelector("#otv").innerHTML = (a * b);
+//             break
+//         }
+//         case '3':{
+//             document.querySelector("#otv").innerHTML = (a ** b);
+//             break
+//         }
+//         case '4':{
+//             document.querySelector("#otv").innerHTML = (a / b);
+//             break
+//         }
+//         case '5':{
+//             document.querySelector("#otv").innerHTML = (a - b);
+//             break
+//         }
+//         case '6':{
+//             document.querySelector("#otv").innerHTML = ((a/100) * b);
+//             break
+//         }
+//         case '7':{
+//             document.querySelector("#otv").innerHTML = (Math.sqrt(a));
+//             break
+//         }
+//     }
+// }
